3c0a87fb7c778efec5b43f0652041c6fcbeb91b7
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2025-10-31 10:46:08 +0900

    update gitignore and FPGA project

---
+++ FPGA/xor_gate_vhdl/xor_gate2.vhd
@@ -0,0 +1,16 @@
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity xor_gate2 is
+	port(A : in std_logic;
+			B : in std_logic;
+			Y : out std_logic;
+			);
+end xor_gate2
+
+
+
+
